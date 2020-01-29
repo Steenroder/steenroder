a7590d402c6cdeca33869d1eb3e8c32e514f7b57
ammedmar <anibal@medina-mardones.com>
2020-01-29 18:55:59 +0100

    improved the comparison routines and started the by_dimension version

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='steenroder',
+      version='0.1',
+      description='Persistence Steenrod squares',
+      url='http://github.com/ammedmar/steenroder',
+      author='Anibal M. Medina-Mardones',
+      author_email='anibal.medinamardones@epfl.ch',
+      license='MIT',
+      packages=['steenroder'],
+      zip_safe=False)
