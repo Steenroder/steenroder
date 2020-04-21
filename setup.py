9501c2ec9f8358e83a1ca4c5a53849afa613ca1b
ammedmar <ammedmar@gmail.com>
2020-04-21 23:12:04 +0200

    Return to core functionalities
    Signed-off-by: ammedmar <ammedmar@gmail.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='steenroder',
-      version='0.1',
-      description='Persistence Steenrod squares',
+      version='0.2',
+      description='Persistence Steenrod modules: the even prime',
       url='http://github.com/ammedmar/steenroder',
       author='Anibal M. Medina-Mardones',
       author_email='anibal.medinamardones@epfl.ch',
--- steenroder/__main__.py
+++
@@ -1,1 +0,0 @@
-from .steenroder import get_coboundary
