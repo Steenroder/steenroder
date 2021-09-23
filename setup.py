1b57e4ff8a32d86c90b38b966d1aa5d6e946f2d7
Umberto Lupo <46537483+ulupo@users.noreply.github.com>
2021-09-23 15:14:17 +0200

    Update author list in setup.py and use new URL

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='steenroder',
+setup(name="steenroder",
       version='0.2',
-      description='Persistence Steenrod modules: the even prime',
-      url='http://github.com/ammedmar/steenroder',
-      author='Anibal M. Medina-Mardones',
-      author_email='anibal.medinamardones@epfl.ch',
-      license='MIT',
-      packages=['steenroder'],
+      description="Persistence Steenrod modules: the even prime",
+      url="http://github.com/Steenroder/steenroder",
+      author="Anibal M. Medina-Mardones and Umberto Lupo",
+      author_email="anibal.medinamardones@epfl.ch, umberto.lupo@epfl.ch",
+      license="MIT",
+      packages=["steenroder"],
       zip_safe=False)
