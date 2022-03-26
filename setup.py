22cf9651a45e2abba100a34013d88f1e91721e5b
Guillaume Tauzin <gtauzin@protonmail.com>
2022-03-26 20:21:47 +0100

    Update setup.py [TODO: example requires]

--- setup.py
+++ setup.py
@@ -1,11 +1,73 @@
-from setuptools import setup
+#! /usr/bin/env python
+"""Computation of persistence Steenrod barcodes"""
 
-setup(name="steenroder",
-      version="0.1.0",
-      description="Persistence Steenrod modules: the even prime",
-      url="http://github.com/Steenroder/steenroder",
-      author="Anibal M. Medina-Mardones and Umberto Lupo",
-      author_email="anibal.medinamardones@epfl.ch, umberto.lupo@epfl.ch",
-      license="MIT",
-      packages=["steenroder"],
-      zip_safe=False)
+import os
+import codecs
+
+
+from pkg_resources.extern.packaging import version
+from setuptools import setup, find_packages
+
+version_file = os.path.join('steenroder', '_version.py')
+with open(version_file) as f:
+    exec(f.read())
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+DISTNAME = 'steenroder'
+DESCRIPTION = 'Computation of persistence Steenrod barcodes '
+with codecs.open('README.rst', encoding='utf-8-sig') as f:
+    LONG_DESCRIPTION = f.read()
+LONG_DESCRIPTION_TYPE = 'text/x-rst'
+MAINTAINER = 'Anibal Medina Mardones, Umberto Lupo'
+MAINTAINER_EMAIL = 'steenroder@gmail.com'
+URL = 'https://github.com/Steenroder/steenroder'
+LICENSE = 'MIT'
+DOWNLOAD_URL = 'https://github.com/Steenroder/steenroder/tarball/v0.1.0'
+VERSION = __version__ # noqa
+CLASSIFIERS = ['Intended Audience :: Science/Research',
+               'Intended Audience :: Developers',
+               'License :: OSI Approved',
+               'Programming Language :: Python',
+               'Topic :: Software Development',
+               'Topic :: Scientific/Engineering',
+               'Operating System :: Microsoft :: Windows',
+               'Operating System :: POSIX',
+               'Operating System :: Unix',
+               'Operating System :: MacOS',
+               'Programming Language :: Python :: 3.6',
+               'Programming Language :: Python :: 3.7',
+               'Programming Language :: Python :: 3.8',
+               'Programming Language :: Python :: 3.9']
+KEYWORDS = 'topological data analysis, persistent ' + \
+    'homology, persistence steenrod modules, persistence ' + \
+    'steenrod barcodes'
+INSTALL_REQUIRES = requirements
+EXTRAS_REQUIRE = {
+      'tests': ['pytest',
+                'pytest-cov',
+                'flake8'],
+      'docs': ['sphinx',
+               'sphinx-issues',
+               'sphinx_rtd_theme',
+               'numpydoc'],
+      'notebooks': []
+}
+
+setup(name=DISTNAME,
+      maintainer=MAINTAINER,
+      maintainer_email=MAINTAINER_EMAIL,
+      description=DESCRIPTION,
+      license=LICENSE,
+      url=URL,
+      version=VERSION,
+      download_url=DOWNLOAD_URL,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESCRIPTION_TYPE,
+      zip_safe=False,
+      classifiers=CLASSIFIERS,
+      packages=find_packages(),
+      keywords=KEYWORDS,
+      install_requires=INSTALL_REQUIRES,
+      extras_require=EXTRAS_REQUIRE)
