b18d2b00c2b9f2d91c043474a28a4e589c3436e1
Guillaume Tauzin <guillaumetauzin.ut@gmail.com>
2022-03-28 19:35:34 +0200

    Fix conf.py
    Signed-off-by: Guillaume Tauzin <guillaumetauzin.ut@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,10 @@
 #
 from steenroder import __version__
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
 
 # -- Project information -----------------------------------------------------
 
